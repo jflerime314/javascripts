39d58818196851bbf5b0840488008f3d1baf3523
Joseph Angelo Flerimé <jflerime@toxiostek.com>
2023-04-30 18:43:21 -0400

    create the server for the index page so...

--- first-animation/index.js
+++ first-animation/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require("path")
 
 const app = express()
+app.use(express.static('public'))
 
 
+function getPagePath(page){
+    return path.join(__dirname + "/" + page)
+}
+app.get('/', (req, res)=>{
+    let pagePath = getPagePath("index.html");
+    return res.sendFile(pagePath);
+})
 
 
 app.listen("3000", ()=>{
